0dfbd2851a5d448628e5aeb891891625153ee543
ryanleekh <RYANLEEKH@GMAIL.COM>
2025-07-23 07:06:13 +0000

    Update

--- app.py
+++ app.py
@@ -159,27 +159,27 @@ def stop_telegram():
     
     return(render_template("telegram.html", status=status))
 
-@app.route("/user_log",methods=["GET","POST"])
-def user_log():
-    #conn = sqlite3.connect("user.db")
-    #c = conn.cursor()
-    #c.execute('''select * from user''')
-    r="Test"
-    #for row in c:
-     # print(row)
-     # r = r + str(row)
-    #c.close()
-    #conn.close()
-    return render_template("user_log.html", r=r)
+@app.route("/userlog",methods=["GET","POST"])
+def userlog():
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    c.execute('''select * from user''')
+    r=""
+    for row in c:
+      print(row)
+      r = r + str(row)
+    c.close()
+    conn.close()
+    return render_template("userlog.html", r=r)
 
-@app.route("/delete_log",methods=["GET","POST"])
-def delete_log():
+@app.route("/deletelog",methods=["GET","POST"])
+def deletelog():
     conn = sqlite3.connect("user.db")
     cursor = conn.cursor()
     cursor.execute('DELETE FROM user')
     conn.commit()
     conn.close()
-    return render_template("delete_log.html", message="User log deleted successfully.")
+    return render_template("deletelog.html", message="User log deleted successfully.")
 
 if __name__ == "__main__":
     app.run()
